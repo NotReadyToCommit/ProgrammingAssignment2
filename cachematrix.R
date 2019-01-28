a3b4ff22fae5ca288211afad2e20ee77e5e65258
NotReadyToCommit <46759254+NotReadyToCommit@users.noreply.github.com>
2019-01-27 21:51:42 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These to functions output the inverse of the square invertible matrix.
+## Since this can be a time-consuming operation, calling cacheSolve first checks to see if the
+## inverse has already been calculated. If it has, then  the previously cached inverse is returned.
+## if it is not, then the inverse is calculated and then cached and output for the user 
 
-## Write a short comment describing this function
+# these two functions should be used together, one after another. example:
+
+# foo <- [your favorote invertible matrix here]
+# bar <- makeCacheMatrix(foo)
+# the_answer <- cacheSolve(bar)
+
+
+## The makeCacheMatrix function contains four subroutines, each of which is a specific operation on a matrix
+## the output is stored as a list of matrices with the list elements being used to
+
+# 1 store a matrix
+# 2 retrive the a matrix
+# 3 cache the inverse of the matrix
+# 4 retrive the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +33,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function outputs the inverse of the matrix output of our previous function
+## by either displaying a cached answer or by calculating it if the cached version is empty
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
